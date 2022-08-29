8f88aa6dc7fca8a610b42ef25ed3d98f6f8a7f77
alexwhoodoo <alexwhoodoo@gmail.com>
2022-08-29 12:28:45 +0300

    modify java version

--- src/main/java/bootsbrain/ExampleRestController.java
+++ src/main/java/bootsbrain/ExampleRestController.java
@@ -27,7 +27,7 @@ public class ExampleRestController {
             return param2;
         }
     }
-    @RequestMapping(path = "/hello",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(path = "/hello1",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public RestResponse restMethod( String name, String middleName) {
         RestResponse result = new RestResponse();
         result.setParam1(name);

b0265fa19fe2f7489c4b02eecd7bfc45452a1961
alexwhoodoo <alexwhoodoo@gmail.com>
2022-09-04 00:06:28 +0300

    change url to hello10 error

--- src/main/java/bootsbrain/ExampleRestController.java
+++ src/main/java/bootsbrain/ExampleRestController.java
@@ -27,9 +27,9 @@ public class ExampleRestController {
             return param2;
         }
     }
-    @RequestMapping(path = "/hello8",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(path = "/hello10",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public RestResponse restMethod( String name, String middleName) {
-        RestResponse result = new RestResponse();
+        RestResponse result = new RestResponse()
         result.setParam1(name);
         result.setParam2(middleName);
 

452d8f3f069b8b08de88bb6c6ca27ca997b6b6b6
alewhoodoo <alexwhoodoo@gmail.com>
2023-04-17 21:46:45 +0300

    k8s first try

--- src/main/java/bootsbrain/ExampleRestController.java
+++ src/main/java/bootsbrain/ExampleRestController.java
@@ -10,6 +10,28 @@ public class ExampleRestController {
     public static class RestResponse {
         private String param1;
         private String param2;
+        private Long longParam1;
+
+        public Long getLongParam2() {
+            return longParam2;
+        }
+
+        public void setLongParam2(Long longParam2) {
+            this.longParam2 = longParam2;
+        }
+
+        private Long longParam2;
+
+
+        public Long getSum() {
+            return sum;
+        }
+
+        public void setSum(Long sum) {
+            this.sum = sum;
+        }
+
+        private Long sum;
 
         public void setParam1(String param1) {
             this.param1 = param1;
@@ -19,6 +41,12 @@ public class ExampleRestController {
             this.param2 = param2;
         }
 
+        public void setLongParam1(Long longParam1) {
+            this.longParam1 = longParam1;
+        }
+
+
+
         public String getParam1() {
             return param1;
         }
@@ -26,12 +54,35 @@ public class ExampleRestController {
         public String getParam2() {
             return param2;
         }
+
+        public Long getLongParam1() {
+            return longParam1;
+        }
+
     }
     @RequestMapping(path = "/hello9",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse restMethod( String name, String middleName) {
+    public RestResponse restMethod1( String name, String middleName, Long long1, Long long2) {
         RestResponse result = new RestResponse();
         result.setParam1(name);
         result.setParam2(middleName);
+
+        result.setLongParam1(long1);
+        result.setLongParam2(long2);
+
+        Long sum = result.getLongParam1() + result.getLongParam2();
+
+        result.setSum(sum);
+
+
+        return result;
+    }
+
+    @RequestMapping(path = "/hello1",method = RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse restMethod2( String param1, String param2) {
+        RestResponse result = new RestResponse();
+        result.setParam1(param1);
+        result.setParam2(param2);
+        result.setLongParam1(12313l);
 
         return result;
     }
--- src/main/java/bootsbrain/SpringRestLesson.java
+++ src/main/java/bootsbrain/SpringRestLesson.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestLesson {
     public static void main (String [] args) {
+
         SpringApplication.run(SpringRestLesson.class);
+
+
     }
 }
